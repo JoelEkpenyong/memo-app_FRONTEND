c22082ae7d9b86d91894b4d1682e2837c1467f44
JoelEkpenyong <joelekpenyong@gmail.com>
2020-10-20 20:11:18 +0100

    updated styles

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -18,10 +18,10 @@ const SideNav = ({tabs, setTabs, toggleNav, setCurrentTab, setTodos, todos, setS
             </span>
             <div className="hamburger pr-3" style={{fontSize: '2.5rem'}} onClick={toggleNav}>
             <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
-                <g fill="none"><path d="M12 12l7 7" stroke="#2D9CDB" stroke-width="2" stroke-linecap="round" class="il-md-length-15 il-md-duration-4 il-md-delay-0"/>
-                  <path d="M12 12L5 5" stroke="#2D9CDB" stroke-width="2" stroke-linecap="round" class="il-md-length-15 il-md-duration-4 il-md-delay-0"/>
-                  <path d="M12 12l-7 7" stroke="#2D9CDB" stroke-width="2" stroke-linecap="round" class="il-md-length-15 il-md-duration-4 il-md-delay-0"/>
-                  <path d="M12 12l7-7" stroke="#2D9CDB" stroke-width="2" stroke-linecap="round" class="il-md-length-15 il-md-duration-4 il-md-delay-0"/>
+                <g fill="none"><path d="M12 12l7 7" stroke="#2D9CDB" strokeWidth="2" strokeLinecap="round" className="il-md-length-15 il-md-duration-4 il-md-delay-0"/>
+                  <path d="M12 12L5 5" stroke="#2D9CDB" strokeWidth="2" strokeLinecap="round" className="il-md-length-15 il-md-duration-4 il-md-delay-0"/>
+                  <path d="M12 12l-7 7" stroke="#2D9CDB" strokeWidth="2" strokeLinecap="round" className="il-md-length-15 il-md-duration-4 il-md-delay-0"/>
+                  <path d="M12 12l7-7" stroke="#2D9CDB" strokeWidth="2" strokeLinecap="round" className="il-md-length-15 il-md-duration-4 il-md-delay-0"/>
                 </g>
               </svg>
             </div>
@@ -48,7 +48,7 @@ const SideNav = ({tabs, setTabs, toggleNav, setCurrentTab, setTodos, todos, setS
           {showForm && <NewTab setTabs={setTabs} tabs={tabs} setTodos={setTodos} todos={todos} setCurrentTab={setCurrentTab} getList={getList}  />}
         </div>
           <TabLists tabs={tabs} toggleNav={toggleNav} setCurrentTab={setCurrentTab} setTodos={setTodos} todos={todos} setShowAll={setShowAll} showForm={showForm}/>
-        <button className="logout_btn mt-auto mb-5" onClick={auth.logout}>
+        <button className="logout_btn mt-auto" onClick={auth.logout} style={{position: 'absolute', bottom: '2rem'}}>
         <svg className="mr-3" width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" clipRule="evenodd" d="M3.96057 3.70801C4.20469 3.95205 4.20476 4.34778 3.96072 4.59189C3.26162 5.29122 2.78558 6.18214 2.59278 7.152C2.39999 8.12186 2.4991 9.12711 2.87759 10.0406C3.25607 10.9542 3.89694 11.735 4.71915 12.2843C5.54136 12.8336 6.50799 13.1269 7.49683 13.1269C8.48567 13.1269 9.4523 12.8336 10.2745 12.2843C11.0967 11.735 11.7376 10.9542 12.1161 10.0406C12.4946 9.12711 12.5937 8.12186 12.4009 7.152C12.2081 6.18214 11.732 5.29122 11.0329 4.59189C10.7889 4.34778 10.789 3.95205 11.0331 3.70801C11.2772 3.46397 11.6729 3.46404 11.917 3.70815C12.7908 4.58231 13.3859 5.69596 13.6269 6.90829C13.8679 8.12061 13.744 9.37718 13.2709 10.5191C12.7978 11.661 11.9967 12.637 10.9689 13.3237C9.94117 14.0103 8.73288 14.3769 7.49683 14.3769C6.26078 14.3769 5.05249 14.0103 4.02473 13.3237C2.99696 12.637 2.19589 11.661 1.72278 10.5191C1.24967 9.37718 1.12578 8.12061 1.36677 6.90829C1.60776 5.69596 2.20282 4.58231 3.07669 3.70815C3.32073 3.46404 3.71646 3.46397 3.96057 3.70801Z" fill="#4F4F4F"/>
           <path fillRule="evenodd" clipRule="evenodd" d="M7.5 0.625C7.84518 0.625 8.125 0.904822 8.125 1.25V7.5C8.125 7.84518 7.84518 8.125 7.5 8.125C7.15482 8.125 6.875 7.84518 6.875 7.5V1.25C6.875 0.904822 7.15482 0.625 7.5 0.625Z" fill="#4F4F4F"/>
